203d5faed17ee5a18c9d2ae09dba61d09c75c4d1
Jayzulu <123288284+Jayzulu@users.noreply.github.com>
2023-03-13 12:11:29 +0800

    2 files modified, 3 added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // run `node index.js` in the terminal
+const express = require('express');
+const app = express();
 
-console.log(`Hello Node.js`, process);
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index' , {title: 'My EJS App'});
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
