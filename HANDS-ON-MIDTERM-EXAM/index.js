e2bde737bcf63cd7800e25098d09f678b2c97169
Jayzulu <123288284+Jayzulu@users.noreply.github.com>
2023-03-22 15:46:53 +0800

    1 file modified, 1 added

--- HANDS-ON-MIDTERM-EXAM/index.js
+++ HANDS-ON-MIDTERM-EXAM/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-
-app.listen(3000, function () {
-  console.log('Server is running on port 3000');
+app.get('/', (req, res) => {
+    res.send('My New App!');
 });
+
+app.get('/api/heroes', (req, res) => {
+    res.send(['Superman', 'Iron Man', 'Batman', 'Hulk']);
+});
+
+app.get('/api/heroes/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+
+app.get('/api/heroes/:title/:publisher', (req, res) => {
+    res.send([req.params, req.query]);
+});
+
+
+app.listen(3000, () => console.log('listening on port 3000'));
